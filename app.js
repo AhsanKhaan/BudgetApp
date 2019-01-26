59f21ab8a83a4c455a71b84b70fa423512f965b1
AhsanKhan <ahsankhan.ubit@gmail.com>
2019-01-26 13:18:54 -0700

    creating DS for income and expense

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 //Budget Controller
 var budgetController = (function () {
-    //some code
+ var Income=function(id,description,value){
+    this.id=id;
+    this.description=description;
+    this.value=value;
+ };
+ var Expense=function(id,description,value){
+    this.id=id;
+    this.description=description;
+    this.value=value;
+ };
+ var data={
+     allItems:{
+         exp:[],
+         inc:[]
+     },
+     totals:{
+        exp:0,
+        inc:0
+     }
+ }
 
 
 })();
